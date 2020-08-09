98b3a11ddd6b6990b49a79cb4d5e47742270df06
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-08-09 09:45:52 +0300

    Index page buttons and Partial View (removed Validation due to globalization TODO that)

--- SparkAuto/Startup.cs
+++ SparkAuto/Startup.cs
@@ -1,20 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SparkAuto.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
-using LazZiya.TagHelpers;
-using Microsoft.AspNetCore.Razor.TagHelpers;
+using SparkAuto.Data;
 
 namespace SparkAuto
 {
@@ -43,7 +35,6 @@ namespace SparkAuto
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages().AddRazorRuntimeCompilation();
 
-            services.AddTransient<ITagHelperComponent, LocalizationValidationScriptsTagHelperComponent>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
