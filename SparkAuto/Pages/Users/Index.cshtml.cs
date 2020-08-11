32d54489530c22bcfca70628c40f988bdce82fdf
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-08-11 20:38:44 +0300

    Page Link custom Tag Helper part 3

--- SparkAuto/Pages/Users/Index.cshtml.cs
+++ SparkAuto/Pages/Users/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SparkAuto.Data;
 using SparkAuto.Models;
+using SparkAuto.Models.ViewModels;
+using SparkAuto.Utility;
 
 namespace SparkAuto.Pages.Users
 {
@@ -20,12 +23,30 @@ namespace SparkAuto.Pages.Users
         }
 
         [BindProperty]
-        public List<ApplicationUser> ApplicationUserList { get; set; }
+        public UsersListViewModel UsersListVM { get; set; }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet(int userPage = 1)
         {
-            ApplicationUserList = await _db.ApplicationUser.ToListAsync();
+            var usersList = await _db.ApplicationUser.ToListAsync();
+            UsersListVM = new UsersListViewModel()
+            {
+                ApplicationUserList = usersList,
+                PagingInfo = new PagingInfo()
+                {
+                    CurrentPage = userPage,
+                    ItemsPerPage = SD.PaginationUsersPageSize,
+                    TotalItems = usersList.Count,
+                    UrlParam = "/Users?userPage=:"
+
+                }
+            };
+            UsersListVM.ApplicationUserList = UsersListVM.ApplicationUserList.OrderBy(p => p.Name)
+                .Skip((userPage - 1) * SD.PaginationUsersPageSize)
+                .Take(SD.PaginationUsersPageSize)
+                .ToList();
+
             return Page();
         }
     }
+
 }
