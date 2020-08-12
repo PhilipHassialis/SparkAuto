f3aae9759858cf917eb39f032fbe54501ae453e3
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-08-13 00:39:05 +0300

    Send Emails part 2

--- SparkAuto/Startup.cs
+++ SparkAuto/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SparkAuto.Data;
+using SparkAuto.Email;
 
 namespace SparkAuto
 {
@@ -43,6 +45,8 @@ namespace SparkAuto
             //    options.LogoutPath = $"/Identity/Account/Logout";
             //    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
             //});
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<EmailOptions>(Configuration);
 
 
         }
