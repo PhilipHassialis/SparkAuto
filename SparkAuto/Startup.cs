f6b8e58dd66048f9090a07ae0f12ec9e8da82b63
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-08-13 12:53:07 +0300

    Enable Facebook registration

--- SparkAuto/Startup.cs
+++ SparkAuto/Startup.cs
@@ -47,8 +47,11 @@ namespace SparkAuto
             //});
             services.AddSingleton<IEmailSender, EmailSender>();
             services.Configure<EmailOptions>(Configuration);
-
-
+            services.AddAuthentication().AddFacebook(options => {
+                options.AppId = "268104661050202";
+                options.AppSecret = "bccce0213de813ba62b69ff39a209512";
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
