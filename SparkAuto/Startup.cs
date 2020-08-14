ee27b8b866e6d728af544482adc2708c069a0993
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-08-14 17:38:37 +0300

    Seed Database part 3

--- SparkAuto/Startup.cs
+++ SparkAuto/Startup.cs
@@ -29,7 +29,7 @@ namespace SparkAuto
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            services.AddScoped<IDbInitializer, DbInitializer>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -55,7 +55,7 @@ namespace SparkAuto
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -68,7 +68,7 @@ namespace SparkAuto
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            dbInitializer.Initialize();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
